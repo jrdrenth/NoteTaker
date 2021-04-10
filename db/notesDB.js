8f43c74ec810cafd45dfff8c216f0f1877b3454d
jrdrenth <john.drenth@gmail.com>
2021-04-10 02:23:08 -0700

    Add functionality for NotesDB to create and delete a note to and from the file

--- db/notesDB.js
+++ db/notesDB.js
@@ -4,34 +4,37 @@ const util = require('util');
 // From 09-NodeJS - Activity 28
 const writeFileAsync = util.promisify(fs.writeFile);
 const readFileAsync = util.promisify(fs.readFile);
+const dbFile = 'db/db.json';
 
 class NotesDB {
 
   async getAllNotes() {
-    const dbFile = 'db/db.json';
-    let notes = [];
-
-    try {
-      // Returns the contents of the file as a string in the variable notes
-      // 'utf8' encodes the raw buffer data in human-readable format
-      const fileContents = await readFileAsync(dbFile, 'utf8');
-      
-      // parses the JSON into objects
-      notes = JSON.parse(fileContents);
-
-    } catch (err) {
-      console.log(err);
-    }
+    // Returns the contents of the file as a string in the variable notes
+    // 'utf8' encodes the raw buffer data in human-readable format
+    const fileContents = await readFileAsync(dbFile, 'utf8');
+    
+    // parses the JSON into an array
+    const notes = JSON.parse(fileContents) ?? [];
 
     return notes;
   }
 
-  async createNote() {
-    //await writeFileAsync(path, data);
+  async createNote(note) {
+    if (!note.title || !note.text) {
+      throw new Error('Neither the title nor the text can be blank');
+    }
+
+    const prevNotes = await this.getAllNotes();
+    const newNotes = [...prevNotes, note];
+    await writeFileAsync(dbFile, newNotes);
+
+    return note;
   }
 
   async deleteNote(id) {
-
+    const prevNotes = await this.getAllNotes();
+    const newNotes = prevNotes.filter((note) => note.id !== id);
+    await writeFileAsync(dbFile, newNotes);
   }
 
 }
